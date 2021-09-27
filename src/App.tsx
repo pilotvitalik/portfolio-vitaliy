dc274aecd1093e165cee3fa306e6001a86a34848
Vitaliy <12345678t>
2021-09-27 17:41:55 +0300

    - Change NextJS to ReactJs

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {useDispatch, useSelector} from 'react-redux';
 
 function App() {
-  const dispatch = useDispatch();
-  const num = useSelector(state => state.num.num);
   return (
     <div className="App">
-      <p>{num}</p>
-        <button type="button" onClick={() => {dispatch({type: 'default/increment', payload: 1})}}>+</button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
--- src/store/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux';
-
-import defaultReducer from './defaultSlice';
-
-const rootReducer = combineReducers({
-    num: defaultReducer,
-})
-
-export default rootReducer;
