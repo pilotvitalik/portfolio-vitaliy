b290c331acef342da6824a2060b40bbdab8e0e32
Vitaliy <12345678t>
2021-10-05 16:29:05 +0300

    - added styles for modal with temporary restricting

--- src/views/Modal/Modal.js
+++ src/views/Modal/Modal.js
@@ -6,7 +6,10 @@ function Modal(){
     return (
         <div className={styles.modal}>
             <div className={styles.modal__content}>
-                <p>Модалка</p>
+                <p className={styles.tempClose}>
+                    Сервис будет активен в ближайшее время
+                </p>
+                <Button mainCSS={['mainBtn', 'tmpCloseBtn']} title='Хорошо'></Button>
             </div>
         </div>
     )
