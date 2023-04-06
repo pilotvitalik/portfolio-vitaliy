f23524f00f88317888ea719b53e3efc284535872
vitaliy_kozhushko <itvkip@yandex.ru>
2023-04-06 18:08:57 +0300

    PO-119 Создать общую страницу со списком проектов, у которых есть страница и ссылкой на эти страницы ----- Подключен react-router-dom

---
+++ src/views/TableContents/TableContents.js
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+
+function TableContents() {
+  const listProjects = [
+    {
+      id: 1,
+      title: 'Jira',
+      link: 'todo'
+    },
+    {
+      id: 2,
+      title: 'Wiki',
+      link: 'wiki'
+    },
+    {
+      id: 3,
+      title: 'Telegram projects',
+      link: 'telegram-projects'
+    }
+  ]
+
+  const links = listProjects.map(project =>
+    <li key={project.id.toString()}>
+      <Link to={`${process.env.REACT_APP_PROJECT_URL}/${project.link}/`}>{project.title}</Link>
+    </li>
+  )
+
+  return (
+    <nav>
+      <ul>
+        {links}
+      </ul>
+    </nav>
+  )
+}
+
+export default TableContents;
